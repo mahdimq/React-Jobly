f0b05bc640e30ebd1c8cc5674321b67cfc16547f
Muhammad Qadir <mahdimq@gmail.com>
2020-09-16 22:19:07 +0300

    add ENV variables

--- src/JoblyApi.js
+++ src/JoblyApi.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = process.env.BASE_URL || 'http://localhost:3001';
+const BASE_URL = process.env.BASE_URL || 'https://jobly-mq.herokuapp.com';
 
 class JoblyApi {
 	static async request(endpoint, paramsOrData = {}, verb = 'get') {
